9bd5441fa0721696bc17bcc6c40b1fb6e788fef4
Ben Jones <ben.james.jones@gmail.com>
2021-01-30 18:39:20 -0700

    more reduced

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,193 @@
-import WebMidi from 'webmidi'
-import SelectorControl from '../svgs/selectorControl.svg'
-import SliderControl from '../svgs/sliderControl.svg'
+//contents of selectorControl.svg;
+const SelectorControl = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:xlink="http://www.w3.org/1999/xlink"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   width="130.43257mm"
+   height="62.694218mm"
+   viewBox="0 0 130.43257 62.694217"
+   version="1.1"
+   id="svg903"
+   inkscape:version="1.0.2 (e86c8708, 2021-01-15)"
+   sodipodi:docname="selectorControl.svg">
+  <style
+     id="style1466">.selectedOption {  }
+</style>
+  <defs
+     id="defs897">
+    <linearGradient
+       inkscape:collect="always"
+       id="linearGradient1482">
+      <stop
+         style="stop-color:#000000;stop-opacity:1"
+         offset="0"
+         id="stop1478" />
+      <stop
+         style="stop-color:#616161;stop-opacity:1"
+         offset="1"
+         id="stop1480" />
+    </linearGradient>
+    <linearGradient
+       inkscape:collect="always"
+       xlink:href="#linearGradient1482"
+       id="linearGradient1488"
+       x1="129.35449"
+       y1="118.09583"
+       x2="156.82431"
+       y2="156.5983"
+       gradientUnits="userSpaceOnUse"
+       gradientTransform="matrix(1.0003537,0,0,0.68448596,-0.03622606,21.297008)" />
+  </defs>
+  <sodipodi:namedview
+     id="base"
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1.0"
+     inkscape:pageopacity="0.0"
+     inkscape:pageshadow="2"
+     inkscape:zoom="1.3029611"
+     inkscape:cx="208.76148"
+     inkscape:cy="205.31627"
+     inkscape:document-units="mm"
+     inkscape:current-layer="layer1"
+     inkscape:document-rotation="0"
+     showgrid="false"
+     inkscape:window-width="1493"
+     inkscape:window-height="943"
+     inkscape:window-x="32"
+     inkscape:window-y="82"
+     inkscape:window-maximized="0"
+     fit-margin-top="0"
+     fit-margin-left="0"
+     fit-margin-right="0"
+     fit-margin-bottom="0" />
+  <metadata
+     id="metadata900">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+        <dc:title></dc:title>
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1"
+     transform="translate(-37.215261,-61.097734)">
+    <rect
+       style="fill:url(#linearGradient1488);fill-opacity:1;stroke:#ff9f00;stroke-width:1.119;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+       id="rect1476"
+       width="129.31357"
+       height="61.575218"
+       x="37.774761"
+       y="61.657234"
+       ry="3.67838" />
+  </g>
+</svg>
+
+`
+//contents of sliderControl.svg
+const SliderControl = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:xlink="http://www.w3.org/1999/xlink"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   width="81.745033mm"
+   height="186.55022mm"
+   viewBox="0 0 81.745033 186.55022"
+   version="1.1"
+   id="svg8"
+   inkscape:version="1.0.2 (e86c8708, 2021-01-15)"
+   sodipodi:docname="sliderControl.svg">
+  <defs
+     id="defs2">
+    <linearGradient
+       inkscape:collect="always"
+       id="linearGradient2746">
+      <stop
+         style="stop-color:#000000;stop-opacity:1"
+         offset="0"
+         id="stop2742" />
+      <stop
+         style="stop-color:#616161;stop-opacity:1"
+         offset="1"
+         id="stop2744" />
+    </linearGradient>
+    <linearGradient
+       inkscape:collect="always"
+       xlink:href="#linearGradient2746"
+       id="linearGradient2748"
+       x1="92.84964"
+       y1="143.25298"
+       x2="136.82738"
+       y2="237.36905"
+       gradientUnits="userSpaceOnUse"
+       gradientTransform="matrix(1.0308319,0,0,1.1553781,-1.8123506,-36.910892)" />
+  </defs>
+  <sodipodi:namedview
+     id="base"
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1.0"
+     inkscape:pageopacity="0.0"
+     inkscape:pageshadow="2"
+     inkscape:zoom="0.86918226"
+     inkscape:cx="362.25687"
+     inkscape:cy="317.49372"
+     inkscape:document-units="mm"
+     inkscape:current-layer="layer1"
+     inkscape:document-rotation="0"
+     showgrid="false"
+     inkscape:window-width="1496"
+     inkscape:window-height="943"
+     inkscape:window-x="0"
+     inkscape:window-y="25"
+     inkscape:window-maximized="0" />
+  <metadata
+     id="metadata5">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+        <dc:title />
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1"
+     transform="translate(-58.618902,-51.920145)">
+    <rect
+       style="fill:url(#linearGradient2748);fill-opacity:1;stroke:#ff9f00;stroke-width:2.26054;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+       id="rect2740"
+       width="79.484497"
+       height="184.28967"
+       x="59.749172"
+       y="53.050415"
+       ry="10.274899"
+       rx="0" />
+  </g>
+</svg>
+
+`
 
 const imageToUseMap = {
     "selectorControl.svg": SelectorControl,
